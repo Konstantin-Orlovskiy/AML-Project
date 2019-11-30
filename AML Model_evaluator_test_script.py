4b7a00981fc552d90d37d90d9fffcf42da3aa1b9
Sdtasker <46995507+Sdtasker@users.noreply.github.com>
2019-11-30 15:19:16 +0000

    Add files via upload
    A test script to check the function is working

---
+++ AML Model_evaluator_test_script.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+#you need the pima_indians.csv to run
+
+#label data
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age','label']
+filename = 'pima-indians-diabetes.data.csv'
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
+
+#read data
+data = read_csv(filename, names=col_names)
+X = data[feature_cols]
+y = data.label
+
+# split data and train model
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+logreg = LogisticRegression()
+
+# fit model
+logreg.fit(X_train, y_train)
+
+# make class predictions for the testing set
+y_pred_class = logreg.predict(X_test)
+y_pred_prob = logreg.predict_proba(X_test)[:, 1]
+
+#pass true and predicted values into dataframe
+df = y_test.to_frame().reset_index()
+df['predicted'] = y_pred_class
+df['probabilty'] = y_pred_prob
+
+df.head()
+
+model_evaluator(df)
